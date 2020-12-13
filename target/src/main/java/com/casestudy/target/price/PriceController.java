34e4fa4ec70f69512cb93e7c86bc1287d2d9a6d3
Jorge Espinosa <espinosa.je@gmail.com>
2020-12-13 01:33:15 -0700

    Fixed the Postman CORS issue for the Price service.

--- target/src/main/java/com/casestudy/target/price/PriceController.java
+++ target/src/main/java/com/casestudy/target/price/PriceController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/product")
 public class PriceController {
@@ -30,7 +31,7 @@ public class PriceController {
 
 		return price;
 	}
-	 // this POST one should be the one used at the end, starting with the GET one because it's easier
+
 	 @PostMapping("/price/addPrice")	
 	    public ResponseEntity<Price> addName(@RequestBody Price productName) {
 
