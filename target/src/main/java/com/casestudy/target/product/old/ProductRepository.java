12a290755705ab22cf8397cc30be45d4180d3e2c
Jorge Espinosa <espinosa.je@gmail.com>
2020-12-17 07:52:19 -0700

    Remove H2 connection and objects, these were used for initial testing

--- target/src/main/java/com/casestudy/target/product/old/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.casestudy.target.product.old;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// we are using Long because Product ID is Long
-public interface ProductRepository extends JpaRepository<Product, Long>{
-	Product findByName(String name);
-}
--- target/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into product(id,name)
-values(10001,'Nindento Switch');
-insert into product(id,name)
-values(10002,'Legos');
-insert into product(id,name)
-values(10003,'Stroller');
-insert into product(id,name)
-values(10004,'Starbucks');
